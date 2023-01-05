9c1dfaf2ea56d3855b0e06891197af729f5cd1ec
dani <danielasofialobo@hotmail.com>
2023-01-05 15:51:54 +0000

    Updates

---
+++ precache-manifest.39f11a5e46d82db1813e55db8ec096a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e0e86c43ccbeedd31b8",
+    "url": "/static/css/main.fc2e062f.chunk.css"
+  },
+  {
+    "revision": "1e0e86c43ccbeedd31b8",
+    "url": "/static/js/main.27c04c1c.chunk.js"
+  },
+  {
+    "revision": "1209b5989bd700f9325c",
+    "url": "/static/js/runtime~main.e8da6e7d.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e5e264ebcafafa576f06b4837161f3b7",
+    "url": "/index.html"
+  }
+];
